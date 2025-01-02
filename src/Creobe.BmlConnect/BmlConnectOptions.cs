aebd40e0645c503535bac91783dd44de34736ae2
Musab Shareef <musab.is@live.com>
2025-01-02 12:59:14 +0500

    fix: Change properties in BmlConnectOptions from init-only to settable and update version to 1.0.1

--- src/Creobe.BmlConnect/BmlConnectOptions.cs
+++ src/Creobe.BmlConnect/BmlConnectOptions.cs
@@ -2,9 +2,9 @@ namespace Creobe.BmlConnect;
 
 public sealed record BmlConnectOptions
 {
-    public required string ApiVersion { get; init; }
-    public required string AppId { get; init; }
-    public required string ApiKey { get; init; }
-    public required string AppVersion { get; init; }
-    public required string Endpoint { get; init; }
+    public required string ApiVersion { get; set; }
+    public required string AppId { get; set; }
+    public required string ApiKey { get; set; }
+    public required string AppVersion { get; set; }
+    public required string Endpoint { get; set; }
 }
